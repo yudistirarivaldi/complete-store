9991e6d2a75a023032c6d21fcd039af91e8d458a
yudistirarivaldi <yudistirarivaldii1234@gmail.com>
2021-08-13 10:11:02 +0800

    complete

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>@yield('title')</title>
+
+    <!-- style css pindah ke css.blade -->
+    @stack('prepend-style')
+    @include('includes.style')
+    @stack('addon-style')
+
+
+
+  </head>
+
+  <body>
+    <!-- Navbar pindah ke navbar.blade--> 
+    @include('includes.navbar-auth')
+    
+
+    <!-- Page Content pindah ke home.blade -->
+    @yield('content')
+
+    <!-- footer pindah ke footer.blade -->
+    @include('includes.footer')
+
+    <!-- Script pindah ke script blade -->
+    @stack('prepend-script')
+    @include('includes.script')
+    @stack('addon-script')
+    
+    
+  </body>
+</html>
---
+++ resources/views/pages/dashboard-settings.blade.php
@@ -0,0 +1,107 @@
+@extends('layouts.dashboard')
+
+
+@section('title')
+    Store Settings
+@endsection
+
+
+@section('content')
+<div class="section-content section-dashboard-home" ata-aos="fade-up">
+  <div class="container-fluid">
+    <div class="dashboard-heading">
+      <h2 class="dashboard-title">Store Settings</h2>
+      <p class="dashboard-subtitle">
+        Make store that profitable
+      </p>
+    </div>
+
+      <div class="dashboard-content">
+        <div class="row">
+          <div class="col-12">
+            <form action="{{ route('dashboard-settings-redirect', 'dashboard-settings-store') }}" method="POST" enctype="multipart/form-data">
+              @csrf
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Nama Toko</label>
+                        <input type="text" class="form-control" name="store_name" value="{{ $user->store_name }}"/>
+                      </div>
+                    </div>
+
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Kategori</label>
+                        <select name="categories_id" class="form-control">
+                          <option value="{{ $user->categories_id }}">Tidak diganti</option>
+                          @foreach ($categories as $category)
+                              <option value="{{ $category->id }}">{{ $category->name }}</option>
+                          @endforeach
+                      </select> 
+                      </div>
+                    </div>
+
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Store</label>
+                        <p class="text-muted">
+                          Apakah anda juga ingin membuka toko?
+                        </p>
+                        <div
+                          class="custom-control custom-radio custom-control-inline"
+                        >
+                          <input
+                            class="custom-control-input"
+                            type="radio"
+                            name="store_status"
+                            id="openStoreTrue"
+                            value="1"
+                            {{ $user->store_status == 1 ? 'checked' : '' }}
+                          />
+                          <label class="custom-control-label" for="openStoreTrue"
+                            >Buka</label
+                          >
+                        </div>
+                        <div
+                          class="custom-control custom-radio custom-control-inline"
+                        >
+                          <input
+                            class="custom-control-input"
+                            type="radio"
+                            name="is_store_open"
+                            id="openStoreFalse"
+                            value="0"
+                            {{ $user->store_status == 0 || $user->store_status == NULL ? 'checked' : '' }}
+                          />
+                          <label
+                            makasih
+                            class="custom-control-label"
+                            for="openStoreFalse"
+                            >Sementara Tutup</label
+                          >
+                        </div>
+                        
+                      </div>
+                      
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col text-right">
+                      <button type="submit" class="btn btn-success px-5">Save Now</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        
+      </div>
+      
+  </div>
+</div>
+     
+
+@endsection
